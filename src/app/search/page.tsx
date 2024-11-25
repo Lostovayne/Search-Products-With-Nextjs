1d6b06f9dfac441f5b76850d796ec67a0c3c62de
Lostovayne <moises.barrera.dev@gmail.com>
2024-11-24 21:34:14 -0300

    agrega la logica de la query

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,4 +1,20 @@
-const Page = () => {
+import { redirect } from "next/navigation";
+
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = ({ searchParams }: PageProps) => {
+  const query = searchParams.query;
+
+  if (!query || Array.isArray(query)) {
+    return redirect("/");
+  }
+
+  // querying logic
+
   return <p>Hello</p>;
 };
 
